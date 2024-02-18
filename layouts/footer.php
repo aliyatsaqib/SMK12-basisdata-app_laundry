355dfdea657bdca06ff3fd1d65f57177df9955a6
aliyatsaqib <aliyatsaa06@gmail.com>
2024-02-18 22:43:01 +0700

    tambahan

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,21 +1,21 @@
 <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Aplikasi Pengelolaan Laundry</a>.</strong> All rights reserved.
-        </footer>
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
     </div>
-    <!-- ./wrapper -->
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Aplikasi Pengelolaan Laundry</a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
 
-    <!-- REQUIRED SCRIPTS -->
+<!-- REQUIRED SCRIPTS -->
 
-    <!-- jQuery -->
-    <script src="../assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../assets/dist/js/adminlte.min.js"></script>
+<!-- jQuery -->
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
